4fbee26bf9904ed6c3b656872ac949d0e00a0780
alejandroalfonsoyero <47833375+alejandroalfonsoyero@users.noreply.github.com>
2019-02-20 16:31:42 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A package for controll step motors with parallel port",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/alejandroalfonsoyero/stepmotor",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
