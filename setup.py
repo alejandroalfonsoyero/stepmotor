9063133f981a25aa4458adce0e29c3314043cab7
alejandroalfonsoyero <47833375+alejandroalfonsoyero@users.noreply.github.com>
2019-02-22 15:40:17 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="stepmotor-alejandroalfonso1994",
+    name="stepmotor",
     version="0.0.1",
     author="Alejandro Alfonso Yero",
     author_email="alejandroalfonso1994@gmail.com",
